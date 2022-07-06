be11e3f92deb98145fcd26b2fbae4ba2b1c788ae
backwardsbanana <93897257+backwardsbanana@users.noreply.github.com>
2022-07-06 01:39:14 +0000

    Add files via upload

---
+++ ica3.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+frame = pd.read_csv("lego_setsHB.csv", usecols=["list_price", "prod_id", "review_difficulty"])
+price = {}
+difficulty = {}
+for i in range(1000):
+	price[frame.at[i, "prod_id"]] = frame.at[i, "list_price"]
+	difficulty[frame.at[i, "prod_id"]] = frame.at[i, "review_difficulty"]
+
+print(price)
+print(difficulty)
+
+#Only 735 unique prod_id's 
