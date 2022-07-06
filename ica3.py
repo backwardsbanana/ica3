4e20937ad1ef16c382447acc6987416c9bcd4262
backwardsbanana <93897257+backwardsbanana@users.noreply.github.com>
2022-07-06 05:33:17 +0000

    Update ica3.py

--- ica3.py
+++ ica3.py
@@ -3,12 +3,20 @@ import pandas as pd
 frame = pd.read_csv("lego_setsHB.csv", usecols=["list_price", "prod_id", "review_difficulty"])
 price = {}
 difficulty = {}
+price2 = {}
+difficulty2 = {}
 for i in range(1000):
 	price[frame.at[i, "prod_id"]] = frame.at[i, "list_price"]
 	difficulty[frame.at[i, "prod_id"]] = frame.at[i, "review_difficulty"]
+	price2.setdefault(frame.at[i, "prod_id"], []).append(frame.at[i, "list_price"])
+	difficulty2.setdefault(frame.at[i, "prod_id"], []).append(frame.at[i, "review_difficulty"])
 
+#736 unique prod_ids
 print(price)
 print("\n")
 print(difficulty)
-
-#Only 736 unique prod_id's 
+print("\n--------------------------\n")
+#All values per prod_id entry
+print(price2)
+print("\n")
+print(difficulty2)
